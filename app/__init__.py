1ed74f3d14b4cdd3e891adeb93d999967b6fa91e
Sebastian Teslic <steslic@diadmeyer.rad.rutgers.edu>
2025-09-04 14:05:06 -0400

    Save local changes before rebase

--- app/__init__.py
+++ app/__init__.py
@@ -10,20 +10,27 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
+migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key_here'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/ecommerce_db'
+    # app.config['SECRET_KEY'] = 'your_secret_key_here'
+    app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "fallback_secret")
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/ecommerce_db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
-    migrate = Migrate(app, db)
+    # migrate = Migrate(app, db)
+    migrate.init_app(app, db)
 
     login_manager.login_view = 'auth.login'
     login_manager.login_message_category = 'info'
